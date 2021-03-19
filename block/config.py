d9857a02ac6d8bdfad3c946c4444241223ca9895
giuppox <61784957+Giuppox@users.noreply.github.com>
2021-03-19 23:22:08 +0100

    Moved `config.py` from using `pkg_resources` to faster `importlib`/`importlib_metadata`

--- block/config.py
+++ block/config.py
@@ -5,12 +5,14 @@ Exposes:
     VERSION (tuple[str]): the version of the package
 """
 from typing import Tuple
-from pkg_resources import get_distribution
-
-pkg = get_distribution('block')
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package.
+    import importlib_metadata as metadata
 
 # Package name and version.
-NAME: str = pkg.project_name
-VERSION: Tuple[str] = tuple(pkg.version.split('.'))
-
-del pkg
+NAME: str = 'block'
+VERSION: Tuple[str] = tuple(
+    metadata.version(NAME).split('.')
+    )
