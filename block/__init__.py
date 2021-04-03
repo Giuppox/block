1112ea8c61569e89bea9f60abea8adfff8768981
giuppox <61784957+Giuppox@users.noreply.github.com>
2021-04-03 10:14:58 +0200

    DOC, MAINT: Removed useless declarations in `_config.py` generated file.

--- block/__init__.py
+++ block/__init__.py
@@ -1,6 +1,4 @@
 from ._config import (
     NAME as __name__, VERSION as __version__, AUTHOR as __author__,
-    EMAIL as __email__, DESCRIPTION as __description__, URLS as __urls__,
-    LONG_DESCRIPTION as __long_description__, CLASSIFIERS as __classifiers__,
-    LICENSE as __license__,
+    EMAIL as __email__, DESCRIPTION as __description__
     )
