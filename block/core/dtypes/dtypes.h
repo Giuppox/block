1afd44c416157519b8482367f30545ab75c40b01
giuppox <61784957+Giuppox@users.noreply.github.com>
2021-04-10 10:10:27 +0200

    ENH: Added dtype object macro definitions

--- block/core/dtypes/dtypes.h
+++ block/core/dtypes/dtypes.h
@@ -4,10 +4,46 @@
 extern "C" {
 #endif
 
-typedef struct {
-    PyObject_HEAD
-    double value;
-} BLK_double_object;
+
+// Define macros for integers data types.
+#define INTEGERS(M)               \
+    M(signed char, CHAR)          \
+    M(unsigned char, UCHAR)       \
+    M(int, INT)                   \
+    M(unsigned int, UINT)         \
+    M(short, SHORT)               \
+    M(unsigned short, USHORT)     \
+    M(long, LONG)                 \
+    M(unsigned long, ULONG)
+
+// Define macros for floating points data types.
+#define FLOATING_POINTS(M)        \
+    M(float, FLOAT)               \
+    M(double, DOUBLE)             \
+    M(long double, LONGDOUBLE)
+
+// Define macros for complex floating points data types.
+#define COMPLEX(M)                                \
+    M(float _Complex, FLOATCOMPLEX)               \
+    M(double _Complex, DOUBLECOMPLEX)             \
+    M(long double _Complex, LONGDOUBLECOMPLEX)
+
+// Define dtypes object macro definition. Doing `DTYPE(int, INT)` is
+// equivalent of doing:
+// ```C
+// typedef struct {
+//     int value;
+// } BLK_INT_object;
+// ```
+#define DTYPE(dtype, name)                  \
+    typedef struct {                        \
+        dtype value;                        \
+    } BLK_##name##_object;
+
+INTEGERS(DTYPE);
+FLOATING_POINTS(DTYPE);
+COMPLEX(DTYPE);
+
 
 #ifdef __cplusplus
 }
