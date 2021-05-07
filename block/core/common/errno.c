ad8815a0977ce3053b5764f441f5d89c3ff8e07e
giuppox <61784957+Giuppox@users.noreply.github.com>
2021-05-07 20:53:35 +0200

    ENH, MAINT: Completed errno working principle

--- block/core/common/errno.c
+++ block/core/common/errno.c
@@ -16,9 +16,14 @@
 
 // Implement `BlkErrNo_New` function, for creating new `BlkErrNo` block error
 // codes.
-BlkErrNo *BlkErrNo_New(
-    BlkErrNo_Type type, char *(*repr)(BlkErrNo *, char **),
-    const char *file, unsigned int line, unsigned int column) {
+BlkErrNo *BlkErrNo_New(BlkErrNo_Type type, const char *file, unsigned int line,
+    unsigned int column, char *(*repr)(BlkErrNo *, char **, char *)) {
+
+    // If `NULL` is passed instead of a representation function uses
+    // `BlkErrNo_Repr` as default.
+    if (repr == NULL) {
+        repr = BlkErrNo_Repr;
+    }
 
     // Allocate errno's memory using `malloc`.
     BlkErrNo *errno = (BlkErrNo *) malloc(sizeof(BlkErrNo));
@@ -26,39 +31,58 @@ BlkErrNo *BlkErrNo_New(
         return NULL;
     }
 
+    // Add properties to `errno`.
     #define PROP(NAME, VALUE) errno->NAME = VALUE;
     PROP(type, type)
     PROP(file, file)
     PROP(line, line)
     PROP(column, column)
     PROP(repr, repr)
+    PROP(throw, &BlkErrNo_Throw)
     #undef PROP
 
     return errno;
 
 }
 
-// Implements `BlkErrNo_Prefix`.
-char *BlkErrNo_Prefix(BlkErrNo *self, char **str) {
-    char *fmt = "%s:%i:%i %s";
-    int size = snprintf(NULL, 0, fmt, self->file, self->line, self->column,
-        BlkErrNo_Type_Strings[self->type]) + 1;
-    *str = malloc(size);
-    if (*str == NULL) {
-        free(*str);
+// Implements `BlkErrNo_Repr`.
+#define PARAMS self->file, self->line, self->column, \
+    BlkErrNo_Type_Strings[self->type], content, line_content
+char *BlkErrNo_Repr(BlkErrNo *self, char **str, char *content) {
+
+    // Read line `self->line` of file `self->file`.
+    FILE *file;
+    if ((file = fopen(self->file, "r")) == NULL) {
         return NULL;
     }
-    snprintf(*str, size, fmt, self->file, self->line, self->column,
-        BlkErrNo_Type_Strings[self->type]);
+    char line_content[BLK_ERRNO_LINE_SIZE];
+    for (int i = 0; i <= self->line;
+        fgets(line_content, sizeof(line_content), file), i++);
+    fclose(file);
+
+    // Define format string for the error message.
+    char *fmt = "%s:%i:%i %s: %s\n%s";
+    // Get size of the final resultant string.
+    int size = snprintf(NULL, 0, fmt, PARAMS) + 1;
+
+    *str = malloc(size);
+    if (*str == NULL) {
+        return NULL;
+    }
+
+    // Both set `str` value and return it.
+    snprintf(*str, size, fmt, PARAMS);
     return *str;
+
 }
+#undef PARAMS
 
-// Implement `BlkErrNo_Repr_Pass`.
-
-
-int main(void) {
-    BlkErrNo *errno = BlkErrNo_New(Pass, *BlkErrNo_Prefix, "errno.c", 192, 0);
-    char *x;
-    errno->repr(errno, &x);
-    printf("%s\n", x);
+// Implements `BlkErrNo_Throw`.
+void BlkErrNo_Throw(BlkErrNo *self, char *content) {
+    char *repr;
+    self->repr(self, &repr, content);
+    if (self->type != Pass || BLK_THROW_PASS) {
+        printf("%s\n", repr);
+        exit(1);
+    }
 }
--- block/core/common/errno.h
+++ block/core/common/errno.h
@@ -7,9 +7,24 @@
  * Including this header, block public functions (that return an errno) should
  * be in the form:
  * ```C
- * // Add example...
+ * BlkErrNo *sum(int *a, int b) {
+ *     *a += b;
+ *     return (BlkErrNo) BlkErrNo_New(Pass, __FILE__, __LINE__, 0, NULL);
+ * }
  * ```
  */
+
+// Define maximum length of sample lengths in error messages.
+#ifndef BLK_ERRNO_LINE_SIZE
+#define BLK_ERRNO_LINE_SIZE 80
+#endif
+
+// Specify whether to raise error on `Pass` throwing or not.
+#ifndef BLK_THROW_PASS
+#define BLK_THROW_PASS 0
+#else
+#define BLK_THROW_PASS 1
+#endif
 
 /*
  * Declare block error codes. Note that when needed you should create new error
@@ -29,11 +44,11 @@
  *                  conversions, such as from a `char *` containing a `long` to
  *                  an `int`.
  */
-#define ERRNO(M)              \
-    M(Pass)                   \
-    M(Error)                  \
-    M(NotImplementedError)    \
-    M(DeprecationError)       \
+#define ERRNO(M) \
+    M(Pass) \
+    M(Error) \
+    M(NotImplementedError) \
+    M(DeprecationError) \
     M(OverflowError)
 
 // Declare `BlkErrNo_Type` enumeration for error codes
@@ -57,24 +72,41 @@ const char *BlkErrNo_Type_Strings[] = {
 typedef struct BlkErrNo BlkErrNo;
 struct BlkErrNo {
     BlkErrNo_Type type;
-    char *(*repr)(BlkErrNo *, char **);
+    char *(*repr)(BlkErrNo *, char **, char *);
     const char *file;
     unsigned int line, column;
+    void (*throw)(BlkErrNo *, char *);
 };
 
-// Define `BlkErrNo` creation function.
-BlkErrNo *BlkErrNo_New(
-    BlkErrNo_Type type, char *(*repr)(BlkErrNo *, char **),
-    const char *file, unsigned int line, unsigned int column);
+/*
+ * `BlkErrNo` creation function.
+ *
+ * Parameters
+ * ----------
+ * `BlkErrNo_Type type`: the type of the error number.
+ * `char *file`: a string containing the path of the file where the exception
+ *               occours.
+ * `const int line`,
+ * `const int column`: the line and the column where the exception occours. Note
+ *                     that if the error is not related to a specific line or
+ *                     column you can use `0` value to specify a whole section.
+ * `char *(*repr)(BlkErrNo *, char **, char *)`: the representation function of
+ *                                               the exception. If the passed
+ *                                               value is `NULL` use default one
+ *                                               `BlkErrNo_Repr`.
+ */
+BlkErrNo *BlkErrNo_New(BlkErrNo_Type type, const char *file, unsigned int line,
+    unsigned int column, char *(*repr)(BlkErrNo *, char **, char *));
 
-// Get prefix of error code, in the form `filepath:row:col type`.
-// Define a generic representation function for error codes.
-char *BlkErrNo_Prefix(BlkErrNo *self, char **str);
+// Defines default `repr` function for error codes (`BlkErrNo_Type`).
+char *BlkErrNo_Repr(BlkErrNo *self, char **str, char *content);
 
-// Define default `repr` functions for error codes (`BlkErrNo_Type`).
-#define STR(M) char *BlkErrNo_Repr_##M(BlkErrNo *self, char **str);
-ERRNO(STR)
-#undef STR
+// Defines exception throwing, that should be a "method" of `BlkErrNo`.
+void BlkErrNo_Throw(BlkErrNo *self, char *content);
+
+#define THROW(e, content) \
+    BlkErrNo *errno = BlkErrNo_New(e, __FILE__, __LINE__, 0, NULL); \
+    errno->throw(errno, content); \
 
 // Remove `ERRNO` macro to avoid conflicts.
 #undef ERRNO
